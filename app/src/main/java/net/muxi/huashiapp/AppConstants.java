1634d1fdd2f6bc364b7b82c598dba882d339b274
fengminchao <360322537@qq.com>
2016-10-05 14:56:12 +0800

    update readme

--- app/src/main/java/net/muxi/huashiapp/AppConstants.java
+++ app/src/main/java/net/muxi/huashiapp/AppConstants.java
@@ -40,6 +40,7 @@ public class AppConstants {
 
     public static final int WEEKS_LENGTH = 21;
 
+    //服务器端mongdb数据库要求初始必须有门课程
     public static final String INIT_COURSE = "re:从零开始的异世界生活";
 
     public static final String CACHE_DIR = App.getContext().getExternalCacheDir().getAbsolutePath();
