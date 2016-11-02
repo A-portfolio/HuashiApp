0a5d59c6dd4e9d5d86b49dae5c3016d7952bab07
fengminchao <360322537@qq.com>
2016-11-02 14:03:32 +0800

    update timetable

--- app/src/main/java/net/muxi/huashiapp/schedule/CoursesView.java
+++ app/src/main/java/net/muxi/huashiapp/schedule/CoursesView.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import net.muxi.huashiapp.R;
+import net.muxi.huashiapp.common.base.BaseActivity;
 import net.muxi.huashiapp.common.data.Course;
 import net.muxi.huashiapp.common.util.DimensUtil;
 import net.muxi.huashiapp.common.util.TimeTableUtil;
@@ -18,10 +23,12 @@ import java.util.List;
  * Created by ybao on 16/7/27.
  * 点击查看当前位置的所有课程的 view
  */
-public class CoursesView extends LinearLayout{
+public class CoursesView extends HorizontalScrollView{
 
     private List<Course> mCourses;
     private Context mContext;
+    //内容 layout
+    private LinearLayout mContentLayout;
     private TextView[] courseTvs;
     private int mWeek;
     private static final int COURSE_WIDTH = DimensUtil.dp2px(68);
@@ -34,17 +41,28 @@ public class CoursesView extends LinearLayout{
         mCourses = courses;
         mWeek = week;
         courseTvs = new TextView[courses.size()];
-        this.setGravity(Gravity.CENTER);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
             this.setElevation(DimensUtil.dp2px(8));
         }
+        addLinearLayout();
         initView();
+    }
+
+    private void addLinearLayout() {
+        mContentLayout = new LinearLayout(mContext);
+        mContentLayout.setGravity(Gravity.CENTER);
+        FrameLayout.LayoutParams params = new LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+        params.gravity = Gravity.CENTER;
+        this.addView(mContentLayout,params);
     }
 
     private void initView() {
         for (int i = 0;i < mCourses.size(); i++){
             TextView textView = new TextView(mContext);
-            LinearLayout.LayoutParams params = new LayoutParams(
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                     COURSE_WIDTH,
                     COURSE_HEIGHT
             );
@@ -65,7 +83,13 @@ public class CoursesView extends LinearLayout{
             }else {
                 textView.setBackground(getResources().getDrawable(R.drawable.bg_simple_class_gray));
             }
-            this.addView(textView);
+            mContentLayout.addView(textView);
+            mContentLayout.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ((BaseActivity)mContext).onBackPressed();
+                }
+            });
         }
 
     }
