1a32796fc75991494cf90b29b692325f4b83576d
ybao <ybaovv@gmail.com>
2017-01-26 20:13:22 +0800

    + schedulelayout

---
+++ app/src/main/java/net/muxi/huashiapp/ui/schedule/WeekLayout.java
@@ -0,0 +1,54 @@
+package net.muxi.huashiapp.ui.schedule;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import net.muxi.huashiapp.AppConstants;
+import net.muxi.huashiapp.R;
+import net.muxi.huashiapp.common.util.DateUtil;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by ybao on 17/1/26.
+ */
+
+public class WeekLayout extends ScheduleTimeLayout {
+
+    private Context mContext;
+
+    private View[] views;
+
+    public WeekLayout(Context context) {
+        this(context, null);
+    }
+
+    public WeekLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        views = new View[7];
+        initView();
+    }
+
+    private void initView() {
+        List<String> dateList = DateUtil.getTheWeekDate(0);
+        for (int i = 0; i < 7; i++) {
+            views[i] = LayoutInflater.from(mContext).inflate(R.layout.view_weekday, this, false);
+            ((TextView)views[i].findViewById(R.id.tv_weekday)).setText(AppConstants.WEEKDAYS[i]);
+            ((TextView)views[i].findViewById(R.id.tv_date)).setText(dateList.get(i));
+            addView(views[i]);
+        }
+    }
+
+    public void setWeekDate(int distance) {
+        List<String> dateList = DateUtil.getTheWeekDate(distance);
+        for (int i = 0;i < 7;i ++){
+            ((TextView) views[i].findViewById(R.id.tv_date)).setText(dateList.get(i));
+        }
+    }
+
+}
