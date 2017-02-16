c6d29c392a0594228c122793c320c7ea04988058
December1900 <522376693@qq.com>
2017-02-16 20:43:06 +0800

    revise StudyRoomBlankActivity

--- app/src/main/java/net/muxi/huashiapp/ui/studyroom/StudyRoomBlankActivity.java
+++ app/src/main/java/net/muxi/huashiapp/ui/studyroom/StudyRoomBlankActivity.java
@@ -5,11 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.muxi.huashiapp.R;
 import net.muxi.huashiapp.common.base.ToolbarActivity;
+import net.muxi.huashiapp.util.DateUtil;
+
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,32 +22,35 @@ import butterknife.ButterKnife;
 
 public class StudyRoomBlankActivity extends ToolbarActivity {
 
+
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
-    @BindView(R.id.ic_today)
-    ImageView mIcToday;
-    @BindView(R.id.tv_weekday)
-    TextView mTvWeekday;
-    @BindView(R.id.iv_blank)
-    ImageView mIvBlank;
+    @BindView(R.id.tv_today)
+    TextView mTvToday;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studyroom_blank);
+
         ButterKnife.bind(this);
+        setTitle("空闲教室");
+
+        String today = DateUtil.getToday(new Date()) + DateUtil.getWeek(new Date());
+        mTvToday.setText(today);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_studyroom,menu);
+        getMenuInflater().inflate(R.menu.menu_studyroom, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
-        if (itemId == R.id.action_correct){
+        if (itemId == R.id.action_correct) {
             StudyRoomCorrectView studyRoomCorrectView = new StudyRoomCorrectView(StudyRoomBlankActivity.this);
             setContentView(studyRoomCorrectView);
         }
