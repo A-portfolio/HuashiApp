a80bdcac97a0d061aae3800c8ea421fc3c4f6a2b
ybao <ybaovv@gmail.com>
2017-02-28 18:21:17 +0800

    adjust permission manage

--- app/src/main/java/net/muxi/huashiapp/ui/more/LogoutDialog.java
+++ app/src/main/java/net/muxi/huashiapp/ui/more/LogoutDialog.java
@@ -19,82 +19,82 @@ import butterknife.ButterKnife;
 
 public class LogoutDialog extends CenterDialogFragment {
 
-        @BindView(R.id.btn_id_logout)
-        Button mBtnIdLogout;
-        @BindView(R.id.btn_library_logout)
-        Button mBtnLibraryLogout;
-        @BindView(R.id.btn_all_logout)
-        Button mBtnAllLogout;
+    @BindView(R.id.btn_id_logout)
+    Button mBtnIdLogout;
+    @BindView(R.id.btn_library_logout)
+    Button mBtnLibraryLogout;
+    @BindView(R.id.btn_all_logout)
+    Button mBtnAllLogout;
 
 
-        private OnIdClickListener mIdClickListener;
-        private OnLibraryClickListener mLibraryClickListener;
-        private OnAllClickListener mAllClickListener;
+    private OnIdClickListener mIdClickListener;
+    private OnLibraryClickListener mLibraryClickListener;
+    private OnAllClickListener mAllClickListener;
 
-        @NonNull
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_log_out, null);
-            ButterKnife.bind(this, view);
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_log_out, null);
+        ButterKnife.bind(this, view);
 
-            Dialog dialog = createCenterDialog(view);
+        Dialog dialog = createCenterDialog(view);
 
-            initEvent();
-            return dialog;
-        }
-
-        private void initEvent(){
-            mBtnIdLogout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (mIdClickListener != null){
-                        mIdClickListener.OnIdClick();
-                    }
-                }
-            });
-
-            mBtnLibraryLogout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (mLibraryClickListener != null){
-                        mLibraryClickListener.OnLibraryClick();
-                    }
-                }
-            });
-
-            mBtnAllLogout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (mAllClickListener != null){
-                        mAllClickListener.OnAllClick();
-                    }
-                }
-            });
-        }
-
-
-        public void setBtnIdLogout(OnIdClickListener onIdClickListener){
-            mIdClickListener = onIdClickListener;
-        }
-
-        public void setBtnLibraryLogout(OnLibraryClickListener onLibraryClickListener){
-            mLibraryClickListener = onLibraryClickListener;
-        }
-
-        public void setBtnAllLogout(OnAllClickListener onAllClickListener){
-            mAllClickListener = onAllClickListener;
-        }
-
-        public interface OnIdClickListener{
-            public void OnIdClick();
-        }
-
-        public interface OnLibraryClickListener{
-            public void OnLibraryClick();
-        }
-
-        public interface OnAllClickListener{
-            public void OnAllClick();
-        }
+        initEvent();
+        return dialog;
     }
+
+    private void initEvent() {
+        mBtnIdLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mIdClickListener != null) {
+                    mIdClickListener.OnIdClick();
+                }
+            }
+        });
+
+        mBtnLibraryLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mLibraryClickListener != null) {
+                    mLibraryClickListener.OnLibraryClick();
+                }
+            }
+        });
+
+        mBtnAllLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAllClickListener != null) {
+                    mAllClickListener.OnAllClick();
+                }
+            }
+        });
+    }
+
+
+    public void setBtnIdLogout(OnIdClickListener onIdClickListener) {
+        mIdClickListener = onIdClickListener;
+    }
+
+    public void setBtnLibraryLogout(OnLibraryClickListener onLibraryClickListener) {
+        mLibraryClickListener = onLibraryClickListener;
+    }
+
+    public void setBtnAllLogout(OnAllClickListener onAllClickListener) {
+        mAllClickListener = onAllClickListener;
+    }
+
+    public interface OnIdClickListener {
+        public void OnIdClick();
+    }
+
+    public interface OnLibraryClickListener {
+        public void OnLibraryClick();
+    }
+
+    public interface OnAllClickListener {
+        public void OnAllClick();
+    }
+}
 
