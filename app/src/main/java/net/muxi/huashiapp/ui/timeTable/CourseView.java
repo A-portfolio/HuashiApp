f9df03edf9122013ed792b8ef1cba8d7fbefe4fd
kolibreath <734780178@qq.com>
2018-03-15 18:47:09 +0800

    refractor:素质课重叠课程右上角加入小绿点提示

--- app/src/main/java/net/muxi/huashiapp/ui/timeTable/CourseView.java
+++ app/src/main/java/net/muxi/huashiapp/ui/timeTable/CourseView.java
@@ -1,8 +1,13 @@
 package net.muxi.huashiapp.ui.timeTable;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.TextView;
+
+import net.muxi.huashiapp.R;
 
 /**
  * Created by ybao (ybaovv@gmail.com)
@@ -13,22 +18,79 @@ import android.widget.TextView;
 public class CourseView extends TextView{
 
     private String cid;
+    private Context context;
+    private CourseView courseView;
+    private boolean isTipOn;
 
     public CourseView(Context context) {
         this(context,null);
+        this.context = context;
     }
-
     public CourseView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if(isTipOn){
+            //默认的颜色是绿色
+            int color = context.getResources().getColor(R.color.green);
+            setTip(color,canvas);
+        }
     }
 
     public void setCourseId(String id){
         cid = id;
     }
-
     public String getCourseId(){
         return cid;
     }
+
+
+    public void setTip(int color,Canvas canvas){
+        Dot dot = new Dot(color);
+        float cx  = getWidth() - dot.marginRight - dot.radius;
+        float cy = dot.marginTop  + dot.radius;
+
+        Drawable drawableTop = getCompoundDrawables()[1];
+        if(drawableTop!=null){
+            int drawableTopWidth = drawableTop.getIntrinsicWidth();
+            if(drawableTopWidth>0){
+                int dotLeft = getWidth() / 2 + drawableTopWidth / 2;
+                cx = dotLeft + dot.radius;
+            }
+        }
+
+        Paint paint = getPaint();
+        //避免影响原来画笔的颜色
+        int tempColor = paint.getColor();
+
+        paint.setColor(dot.color);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(cx,cy,dot.radius,paint);
+
+        paint.setColor(tempColor);
+    }
+
+    public void setIsTipOn(boolean isTipOn){
+        this.isTipOn = isTipOn;
+    }
+    class Dot{
+        int color;
+        int radius;
+        int marginTop;
+        int marginRight;
+
+        public Dot( int color){
+            float density = context.getResources().getDisplayMetrics().density;
+            radius = (int) (5 * density);
+            marginTop = (int) (3 * density);
+            marginRight = (int) (3 * density);
+            this.color = color;
+        }
+    }
+
 
 
 }
--- app/src/main/java/net/muxi/huashiapp/ui/timeTable/TableContent.java
+++ app/src/main/java/net/muxi/huashiapp/ui/timeTable/TableContent.java
@@ -17,7 +17,9 @@ import net.muxi.huashiapp.util.DimensUtil;
 import net.muxi.huashiapp.util.TimeTableUtil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ybao on 17/1/27.
@@ -25,6 +27,8 @@ import java.util.List;
 
 //在这里设置是否有 非本周字段
 public class TableContent extends FrameLayout {
+    private Map<String ,List<String>> overlapMap = new HashMap<>();
+    private List<String> overlapTags = new ArrayList<>();
     private Scroller mScroller;
     private Context mContext;
     private OnCourseClickListener mOnCourseClickListener;
@@ -85,6 +89,9 @@ public class TableContent extends FrameLayout {
      */
     public void addCourses(List<Course> courseList,int selectWeek) {
         mCourseList.addAll(courseList);
+
+        //查有没有重叠的课程
+        generateMap(courseList);
         for (int i = 0; i < courseList.size(); i++) {
             if (!TimeTableUtil.isThisWeek(selectWeek,courseList.get(i).weeks)) {
                     addCourseView(courseList.get(i),selectWeek,courseList.get(i).weeks);
@@ -107,17 +114,22 @@ public class TableContent extends FrameLayout {
 
 
     public void addCourseView(Course course,int selectWeek,String weekStr) {
+        String tag = generateTag(course);
+        String value = generateValue(course);
 
         List<String > weekList = convert(weekStr.split(","));
         CourseView tvCourse = new CourseView(mContext);
+        checkOverLap(tag,value,tvCourse);
+
         tvCourse.setTextColor(Color.WHITE);
         tvCourse.setTextSize(TypedValue.COMPLEX_UNIT_SP,10);
         tvCourse.setPadding(DimensUtil.dp2px(10),DimensUtil.dp2px(8),DimensUtil.dp2px(10),DimensUtil.dp2px(8));
         LayoutParams courseParams = new LayoutParams(TimeTable.COURSE_WIDTH,
                 course.during * TimeTable.COURSE_TIME_HEIGHT - DimensUtil.dp2px(3));
-        courseParams.setMargins(
-                DimensUtil.dp2px(65 * TimeTableUtil.weekday2num(course.day) + 1),
+        courseParams.setMargins(DimensUtil.dp2px(65 * TimeTableUtil.weekday2num(course.day) + 1),
                 DimensUtil.dp2px(57 * (course.start - 1) + 1), 0, 0);
+
+
         if (TimeTableUtil.isThisWeek(selectWeek,course.weeks)) {
             tvCourse.setBackground(getResources().getDrawable(TimeTableUtil.getCourseBgAccordDay(course.day)));
             tvCourse.setText(ellipseCourse(course.course) + "\n\n@" +
@@ -132,12 +144,20 @@ public class TableContent extends FrameLayout {
         tvCourse.setCourseId(course.id);
         this.addView(tvCourse, courseParams);
 
-        if(selectWeek>Integer.parseInt(weekList.get(0))) {
+
+
+        //{4,5,6,7,8}
+        //假设当前周为3 如果 list中开始的周数为list.get(0) 小于开始周 不会不显示 会显示灰色
+        //假设当前周为10 如果list中开始的周数为list.get(0) 大于开始 但是不在 范围之内会设置不可见 避免重叠的情况
+        if(selectWeek > Integer.parseInt(weekList.get(0))&&selectWeek<Integer.parseInt(weekList
+        .get(weekList.size()-1))) {
             if (!weekList.contains(String.valueOf(selectWeek))) {
+                overlapTags.add(tag);
                 tvCourse.setVisibility(GONE);
-                return;
             }
         }
+
+
         tvCourse.setOnTouchListener((view, motionEvent) -> {
             if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
                 ((TimeTable) getParent().getParent()).setCurDownTarget(view);
@@ -145,6 +165,35 @@ public class TableContent extends FrameLayout {
             return false;
         });
 
+    }
+
+    private void  generateMap(List<Course> courseList){
+        for(Course course:courseList){
+            overlapMap.put(generateTag(course),new ArrayList<>());
+        }
+        for(String key:overlapMap.keySet()){
+            for(Course course:courseList){
+                if(key.equals(generateTag(course))){
+                    overlapMap.get(key).add(generateValue(course));
+                }
+            }
+        }
+    }
+
+    //确定一种相同的生成tag的法则
+    private String generateTag(Course course){
+        return course.getDay()+course.getStart()+course.getDuring();
+    }
+    //确定一种相同的生成value的法则
+    private String generateValue(Course course){
+        return course.getDay()+course.getStart()+course.getDuring()+course.teacher;
+    }
+
+    //查看一下有没有重叠的课程
+    private void checkOverLap(String tag,String value,CourseView courseview){
+       if(overlapMap.get(tag).contains(value)&&overlapMap.get(tag).size()>1){
+           courseview.setIsTipOn(true);
+       }
     }
 
 
