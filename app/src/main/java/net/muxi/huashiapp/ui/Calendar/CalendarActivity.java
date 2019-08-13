ed7ee9b8fbb278c6142232d18eaec8689ea19200
messi-wpy <904315105@qq.com>
2019-08-13 23:47:35 +0800

    calendar

--- app/src/main/java/net/muxi/huashiapp/ui/Calendar/CalendarActivity.java
+++ app/src/main/java/net/muxi/huashiapp/ui/Calendar/CalendarActivity.java
@@ -3,23 +3,39 @@ package net.muxi.huashiapp.ui.Calendar;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PointF;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.ImageViewState;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+import com.facebook.binaryresource.FileBinaryResource;
+import com.facebook.cache.common.SimpleCacheKey;
+import com.facebook.cache.disk.FileCache;
+import com.facebook.common.executors.UiThreadImmediateExecutorService;
+import com.facebook.common.references.CloseableReference;
+import com.facebook.datasource.BaseDataSubscriber;
+import com.facebook.datasource.DataSource;
+import com.facebook.datasource.DataSubscriber;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 import com.facebook.imagepipeline.common.ResizeOptions;
 import com.facebook.imagepipeline.core.ImagePipeline;
+import com.facebook.imagepipeline.memory.PooledByteBuffer;
+import com.facebook.imagepipeline.memory.PooledByteBufferInputStream;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import com.muxistudio.appcommon.appbase.ToolbarActivity;
@@ -29,9 +45,15 @@ import com.muxistudio.appcommon.utils.FrescoUtil;
 import com.muxistudio.common.util.Logger;
 import com.muxistudio.common.util.NetUtil;
 import com.muxistudio.common.util.PreferenceUtil;
+import com.muxistudio.common.util.ToastUtil;
 
 import net.muxi.huashiapp.R;
 import net.muxi.huashiapp.ui.more.ShareDialog;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
@@ -42,9 +64,8 @@ import rx.schedulers.Schedulers;
  */
 public class CalendarActivity extends ToolbarActivity {
 
-    private ScrollView mScrollView;
-    private SimpleDraweeView mDraweeView;
-
+    private SubsamplingScaleImageView mLargeImageView;
+    DataSource<CloseableReference<PooledByteBuffer>> imageSource;
     private String picUrl;
 
 
@@ -62,29 +83,75 @@ public class CalendarActivity extends ToolbarActivity {
        // picUrl=PreferenceUtil.getString(PreferenceUtil.CALENDAR_ADDRESS);
 
         setTitle("校历");
-        loadImage(Uri.parse(picUrl));
 
+        ViewTreeObserver vto = mLargeImageView.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                mLargeImageView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                mLargeImageView.getHeight();
+                int w=mLargeImageView.getWidth();
+                loadLargeImage(Uri.parse(picUrl),w);
 
-
+            }
+        });
 
 
     }
 
 
-
-    private void loadImage(Uri uri){
+    private void loadLargeImage(Uri url,int w){
         clearOldCalendar();
+        FileCache fileCache=Fresco.getImagePipelineFactory().getMainFileCache();
+        SimpleCacheKey cacheKey=new SimpleCacheKey(url.toString());
+        if (fileCache.hasKey(cacheKey)) {
 
-        ImageRequestBuilder builder=ImageRequestBuilder.newBuilderWithSource(uri);
-        //builder.setResizeOptions(new ResizeOptions(760,3246));
+            FileBinaryResource resource = (FileBinaryResource) fileCache.getResource(cacheKey);
+            File file = resource.getFile();
+            mLargeImageView.setImage(ImageSource.uri(Uri.fromFile(file)),new ImageViewState(fiTXY(w,file),new PointF(0,0),0));
+            return;
+        }
+
+
+        ImageRequestBuilder builder=ImageRequestBuilder.newBuilderWithSource(url);
         ImageRequest request=builder.build();
-        DraweeController controller=Fresco.newDraweeControllerBuilder()
-                .setImageRequest(request)
-                .build();
-        mDraweeView.setAspectRatio((float)760/(float)3246);
-        mDraweeView.setController(controller);
+        imageSource=Fresco.getImagePipeline()
+                        .fetchEncodedImage(request,getApplicationContext());
 
+        DataSubscriber<CloseableReference<PooledByteBuffer>>subscriber=new BaseDataSubscriber<CloseableReference<PooledByteBuffer>>() {
+            @Override
+            protected void onNewResultImpl(DataSource<CloseableReference<PooledByteBuffer>> dataSource) {
+                if (!dataSource.isFinished()){
+                    return;
+                }
+
+                FileBinaryResource resource = (FileBinaryResource) fileCache.getResource(cacheKey);
+                File file = resource.getFile();
+                mLargeImageView.setImage(ImageSource.uri(Uri.fromFile(file)),new ImageViewState(fiTXY(w,file),new PointF(0,0),0));
+
+
+
+
+            }
+
+            @Override
+            protected void onFailureImpl(DataSource<CloseableReference<PooledByteBuffer>> dataSource) {
+                ToastUtil.showLong("加载校历出错!");
+            }
+        };
+
+        imageSource.subscribe(subscriber, UiThreadImmediateExecutorService.getInstance());
     }
+
+    public float  fiTXY(int width,File file)  {
+        BitmapFactory.Options option=new BitmapFactory.Options();
+        option.inJustDecodeBounds=true;
+        BitmapFactory.decodeFile(file.getPath(), option);
+        float scale=(float) width/option.outWidth;
+
+        return scale;
+    }
+
 
 
     private void clearOldCalendar(){
@@ -131,7 +198,12 @@ public class CalendarActivity extends ToolbarActivity {
     }
 
     private void initView() {
-        mScrollView = findViewById(R.id.scroll_view);
-        mDraweeView = findViewById(R.id.drawee);
+        mLargeImageView=findViewById(R.id.largeImageView);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+
     }
 }
