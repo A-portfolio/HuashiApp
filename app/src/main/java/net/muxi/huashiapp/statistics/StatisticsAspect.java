ae02f527bb156b73176dc9691383e621c6677f2e
messi-wpy <904315105@qq.com>
2019-09-16 02:04:58 +0800

    aspectj++onclick,web

--- app/src/main/java/net/muxi/huashiapp/statistics/StatisticsAspect.java
+++ app/src/main/java/net/muxi/huashiapp/statistics/StatisticsAspect.java
@@ -1,7 +1,9 @@
 package net.muxi.huashiapp.statistics;
 
+import android.content.res.Resources;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.View;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
@@ -15,11 +17,11 @@ public class StatisticsAspect {
 
     public static final String TAG="statistics";
 
+    //pageView
     //activity打开统计
 
     @Pointcut("execution(* onCreate(..))&&within(com.muxistudio.appcommon.appbase.BaseAppActivity)")
     public void activityCreate(){}
-
     @After("activityCreate()")
     public void afterActivityCreate(JoinPoint point){
         Log.i(TAG, "afterActivityCreate: "+point.getThis().getClass().getSimpleName());
@@ -30,9 +32,9 @@ public class StatisticsAspect {
     //1.统计fragment创建
     @Pointcut("execution(void onCreate(..)) && within(android.support.v4.app.Fragment) && target(fragment)")
     public void fragmentResume(Fragment fragment) {}
-
     @After("fragmentResume(fragment)")
     public void afterFragment(Fragment fragment){
+
         Log.i(TAG, "afterFragment: "+fragment.getClass().getSimpleName());
 
     }
@@ -40,9 +42,9 @@ public class StatisticsAspect {
     //2.统记隐藏打开：
     @Pointcut("execution(void onHiddenChanged(boolean)) && within(android.support.v4.app.Fragment) && target(fragment) && args(hidden)")
     public void onHiddenChanged(Fragment fragment, boolean hidden) {}
-
     @After("onHiddenChanged(fragment,hidden)")
     public void afterFragmentHidden(Fragment fragment,boolean hidden){
+        if (hidden)return;
         Log.i(TAG, "afterFragmentHidden: " + fragment.getClass().getSimpleName());
         Log.i(TAG, "afterFragmentHidden: hidden"+hidden);
 
@@ -51,11 +53,50 @@ public class StatisticsAspect {
     //3.viewpage+fragment转换调用这个方法：
     @Pointcut("execution(void setUserVisibleHint(..)) && within(android.support.v4.app.Fragment) && target(fragment) && args(visible)")
     public void setUserVisibleHint(Fragment fragment, boolean visible) {}
-
     @After("setUserVisibleHint(fragment,visible)")
     public void afterFragmentVisible(Fragment fragment,boolean visible){
+        //主页面的banner一直在显示隐藏fragment，之后要改进，所以这里为了不重复统计先不记录他
+        if (!visible)return;
+        if (fragment.getClass().getSimpleName().equals("CardFragment"))return;
         Log.i(TAG, "afterFragmentVisible: "+fragment.getClass().getSimpleName());
 
     }
+
+
+
+
+    //4.onclick()事件统计：userEvent
+    //因为onclick方法实现的方式不同（匿名内部类,实现接口...所以干脆加两个call和execution）
+    @After("call(void android.view.View.OnClickListener+.onClick(..))  && args(view)")
+    public void afterOnClickAll(View view,JoinPoint joinPoint) {
+        Log.i(TAG, "afterOnClickAll: "+joinPoint.getThis().getClass().getSimpleName());
+        Log.i(TAG, "afterOnClickAll: "+view.getClass().getSimpleName());
+    }
+
+    @After("execution(void android.view.View.OnClickListener+.onClick(..))  && args(view)")
+    public void afterOnClick(View view,JoinPoint joinPoint) {
+        Log.i(TAG, "afterOnClickAll: "+joinPoint.getThis().getClass().getSimpleName());
+        Log.i(TAG, "afterOnClickAll: "+view.getClass().getSimpleName());
+
+        try {
+            Resources res = view.getResources();
+            Log.i(TAG, "afterOnClick: " + res.getResourceEntryName(view.getId()));
+        }catch (Resources.NotFoundException e){
+            Log.e(TAG, "afterOnClick: ",e );
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //5.网页统计
+    @After("execution(* net.muxi.huashiapp.ui.webview.WebViewActivity.newIntent(..))")
+    public void afterOpenWebPage(JoinPoint joinPoint) {
+        Log.i(TAG, "webPage: "+joinPoint.getArgs()[1]);
+    }
+
+    //apiEvent
+
+
+
 
 }
